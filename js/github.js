e8f0a70c51f74b2a0ace8973894731f23987a9c2
HK Kahng <hk@router-rooter.com>
2016-10-14 09:57:34 -0700

    add code to remove access token from API call if .env file or token not available

--- js/github.js
+++ js/github.js
@@ -1,10 +1,14 @@
 var apiKey = require('./../.env').apiKey;
+var accessToken = '';
+if (apiKey) {
+  accessToken = '&access_token=' + apiKey;
+}
 
 GitHub = function(){
 }
 
 GitHub.prototype.getRepos = function(userName, displayFunction) {
-  $.get('https://api.github.com/users/' + userName + '/repos?sort=created&per_page=200&access_token=' + apiKey).then(function(response) {
+  $.get('https://api.github.com/users/' + userName + '/repos?sort=created&per_page=200' + accessToken).then(function(response) {
     displayFunction(response);
   }).fail(function(error) {
     $('#displayRepos').text(error.responseJSON.message);
