3882c1aa25e1857a5d86a88b83a18f15b820b40e
HK Kahng <hk@router-rooter.com>
2016-10-14 08:49:06 -0700

    add code to show creation date of each repository

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,14 +2,14 @@ var GitHub  = require('./../js/github.js').githubModule;
 
 var displayRepos = function(reposList) {
   $('#display_repos').empty();
-  $('#display_repos').append('<li class="list-group-item active">Repository Name / Description</li>');
+  $('#display_repos').append('<li class="list-group-item active">Repository Name / Description (Created On)</li>');
   for (var i = 0; i < reposList.length; i++) {
     var repoName = reposList[i].name;
     var repoDescription = reposList[i].description;
     if (!repoDescription) {
       repoDescription = 'No description given';
     }
-    var repoCreated = reposList[i].created_at;
+    var repoCreated = moment(reposList[i].created_at).format('MM/DD/YYYY');
     var repoURL = reposList[i].html_url;
     var liContent = '<li class="list-group-item"><a href="' + repoURL + '" target="_new">' + repoName + ' / ' + repoDescription + ' (' + repoCreated + ')' + '</a></li>';
     $('#display_repos').append(liContent);
